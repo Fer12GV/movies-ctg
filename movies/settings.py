b6d5868477b99967373f71e94ff25e25aa452279
Luis Moncaris <lmoncarisg@gmail.com>
2019-06-19 16:28:00 -0500

    Permissions Viewsets and Authentication

--- movies/settings.py
+++ movies/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
 
     'debug_toolbar',
     'rest_framework',
+    'rest_framework.authtoken',
 
     'mdb',
     'mdb.api',
@@ -124,8 +125,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    )
 }
+
 
 API_VERSION = 'v1'
 
